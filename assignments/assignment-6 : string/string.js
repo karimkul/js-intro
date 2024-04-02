5e827eedcb5ca57adc457e79d68870cd3d741f36
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-02 15:04:26 -0400

    Finish assignment-6

---
+++ assignments/assignment-6 : string/string.js
@@ -0,0 +1,14 @@
+var name = "Hello, my name is Jamshid";
+var age = "and I am 24 yaers old.";
+var city = "I live in New york city,";
+var profession = "Where i work as a Full stack developer at Google company.";
+var hobby = "I enjoy codding and swimming.";
+var food = "My favorite food is pasta and ";
+var color = "my favorite color is black.";
+var dreamTravel = "My dream is to visit New Zealand.";
+var season =
+    "My favorite season of the year is spring because of the beautiful blooming of the trees.";
+var animal = "My favorite animal is cat.";
+
+var result = `${name} ${age} ${city} ${profession} ${hobby} ${food} ${color} ${dreamTravel} ${season}`;
+console.log(result);
