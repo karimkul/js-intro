f4d139f9071bb05bbd41dfb18581d78b192577bc
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-08 02:55:23 -0400

    Finish assessment-1

---
+++ assessment/assessment-1 : variables/variable.js
@@ -0,0 +1,39 @@
+// 1. Challenge
+var num = 15;
+var num2 = 23;
+
+console.log(num); //15
+console.log(num2); //23
+
+// 2. Challenge
+var secondChallenge = num > num2;
+console.log(secondChallenge); //false
+
+var secondChallenge = num === num2;
+console.log(secondChallenge); //false
+
+// 3. Challenge
+var thirdChallengeMultiply = num * num2;
+console.log(thirdChallengeMultiply); //345
+
+var thirdChallengeDevide = thirdChallengeMultiply / 2;
+console.log(thirdChallengeDevide); //172.5
+
+// 4. Challenge
+var boolean1 = 14 > 13;
+var boolean2 = 21 < 19;
+var booleanLogicalAnd = boolean1 === boolean2;
+console.log(booleanLogicalAnd); //false
+
+var booleanLogicalOr = boolean1 || boolean2;
+console.log(booleanLogicalOr); //true
+
+// 5. Challenge
+var fifthChallenge = 123;
+console.log(typeof fifthChallenge); //number
+
+// 6. Challenge
+var sixthChallegeNumber = 55;
+var sixthChallegeString = "alien";
+var sixthChallegeStrictEquality = sixthChallegeNumber === sixthChallegeString;
+console.log(sixthChallegeStrictEquality); //false
