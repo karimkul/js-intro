1b36341126d682f2f050f4fc20e6b16a6a5de9c8
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-08 03:35:00 -0400

    Finish assessment-2

---
+++ assessment/assessment-2/index.js
@@ -0,0 +1,43 @@
+// 1. Challenge
+var name = "Jamshid";
+var age = 24;
+var programmingExperience = false;
+console.log(name); // Jamshid
+console.log(age); // 24
+console.log(programmingExperience); //fasle
+
+// 2. Challenge
+var name = "Jamshid";
+var age = 24;
+var programmingExperience = false;
+programmingExperience = true;
+console.log(programmingExperience); //true
+
+// 3. Challenge
+var name = "Jamshid";
+var numberOfLatters = 7;
+var nameResult = numberOfLatters > 18;
+console.log(nameResult); //false
+
+var age = 24;
+var ageResult = age > 18;
+console.log(ageResult); //true
+
+// 4. Challenge
+var age = 24;
+age += 1;
+console.log(age); //25
+
+// 5. Challenge
+var likeCoffee = false;
+likeCoffee = !false;
+console.log(likeCoffee); //true
+
+// 6. Challenge
+var numberOfPets = 2;
+var resultNumberOfPets = 1 > numberOfPets;
+console.log(resultNumberOfPets); //false
+
+numberOfPets = "two";
+var resultNumberOfPets = "one" > numberOfPets;
+console.log(resultNumberOfPets); //false
