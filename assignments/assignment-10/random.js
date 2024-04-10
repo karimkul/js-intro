80cb81c9c35eb282d652b881f2b03fd0824b2e7d
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-10 02:38:34 -0400

    Add assignment-10 update

--- assignments/assignment-10/random.js
+++ assignments/assignment-10/random.js
@@ -1,9 +1,9 @@
 // 1. Between 1 and 10
-var problemOne = Math.floor(Math.random() * 10) + 1;
+var problemOne = Math.ceil(Math.random() * 10);
 console.log(problemOne); //Generate between 1 and 10 all includes
 
 // 2. Between 1 and 100
-var problemTwo = Math.floor(Math.random() * 100) + 1;
+var problemTwo = Math.ceil(Math.random() * 100);
 console.log(problemTwo); //Generate between 1 and 100 all includes
 
 // 3. Between 50 and 100
@@ -26,14 +26,14 @@ console.log(problem6); //Generate between 1000 and 2000 all includes
 var problem7 = Math.ceil(Math.random() * -1000);
 console.log(problem7);
 
-// 8. Between 1 and 1000ç
-var problem8 = Math.floor(Math.random() * 1000) + 1;
+// 8. Between 1 and 1000
+var problem8 = Math.ceil(Math.random() * 1000);
 console.log(problem8); //Generate between 1 and 1000 all includes
-
-// 10. Between 10000 and 20000
-var problem10 = Math.floor(Math.random() * (20000 - 10000 + 1)) + 10000;
-console.log(problem10); //Generate between 1000 and 2000 all includes
 
 // 9. Between -500 and 500
 var problem9 = Math.ceil(Math.random() * 1000) - 500;
 console.log(problem9);
+
+// 10. Between 10000 and 20000
+var problem10 = Math.floor(Math.random() * (20000 - 10000 + 1)) + 10000;
+console.log(problem10); //Generate between 1000 and 2000 all includes
