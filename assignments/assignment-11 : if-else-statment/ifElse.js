70b4e0131ace15b3f316b51b5ff003f02f9fc4ee
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-17 00:30:27 -0400

    Update assignment-11

--- assignments/assignment-11 : if-else-statment/ifElse.js
+++ assignments/assignment-11 : if-else-statment/ifElse.js
@@ -1,8 +1,8 @@
 // 1. Password Correctness Check
-var correct = "abdullajon123";
-var correct2 = "Abdullajon123";
+var correctPassword = "abdullajon123";
+var userInputPassword = "Abdullajon123";
 
-if (correct === correct2) {
+if (correctPassword === userInputPassword) {
     console.log("Password is correct");
 } else {
     console.log("Password is incorrect");
@@ -12,26 +12,24 @@ if (correct === correct2) {
 var requiredAge = 18;
 var userAge = 17;
 
-if (userAge > requiredAge) {
+if (userAge >= requiredAge) {
     console.log("You have access to the content.");
 } else console.log("Access restricted. You must be 18 years or older.");
 
 // 3. Checking Input Length
 var userName = "Jami";
-var userNameLength = userName.length;
 
-if (userNameLength > 5) {
+if (userName.length > 5) {
     console.log("Username is valid.");
 } else {
     console.log("Username must be at least 5 characters long.");
 }
 
 // 4. Simple Quiz
-// 100 + 23 = ?
-var correctAnswer = 123;
-var usersAnswer = 130;
+var correctAnswer = "123";
+var usersAnswer = "123";
 
-if (correctAnswer === usersAnswer) {
+if (usersAnswer.toLowerCase() === correctAnswer.toUpperCase()) {
     console.log("Correct! You may proceed.");
 } else {
     console.log("Incorrect answer. Please try again.");
