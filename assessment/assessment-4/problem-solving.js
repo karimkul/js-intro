6f8ccc02e8d250730bb1b09a6ed1509fff5addda
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-22 00:13:50 -0400

    Update assessment-4

--- assessment/assessment-4/problem-solving.js
+++ assessment/assessment-4/problem-solving.js
@@ -1,35 +1,36 @@
 // 1. Challenge
 var singleWord = "heaven";
-var generatednum1 = Math.ceil(Math.random() * singleWord.length - 1);
-var generatednum2 = Math.ceil(Math.random() * singleWord.length - 1);
-var compareRandomStringLength = generatednum1 === generatednum2;
-console.log(compareRandomStringLength); // true and false
+var index1 = Math.floor(Math.random() * singleWord.length);
+var index2 = Math.floor(Math.random() * singleWord.length);
+var char1 = singleWord[index1].toUpperCase();
+var char2 = singleWord[index2].toUpperCase();
 
-var character1 = singleWord[generatednum1].toUpperCase();
-var character2 = singleWord[generatednum2].toUpperCase();
-console.log(character1);
-console.log(character2);
+if (char1 === char2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+console.log(char1 + char2);
 
 // 2. Challenge
 var randomNumber1 = Math.ceil(Math.random() * 1000);
 console.log(randomNumber1);
-var oddNum = randomNumber1 % 2 === 0;
-var evenNum = randomNumber1 % 2 === 1;
+var oddNum = randomNumber1 % 2 !== 0;
+var evenNum = randomNumber1 % 2 === 0;
 console.log(oddNum);
 console.log(evenNum);
 
 // 3. Challenge
-var numerator = Math.floor(Math.random() * -100);
+var numerator = Math.floor(Math.random() * (-100 + 1) - 1);
 var denominator = Math.floor(Math.random() * 4) + 5;
-var devidedResult = Math.floor(numerator / denominator);
-var roundedResult = Math.floor(Math.round(devidedResult));
-console.log(devidedResult);
-console.log(roundedResult);
+var result = numerator / denominator;
+console.log("Original result: " + result);
+console.log("Rounded result: " + Math.round(result));
 
 // 4. Challenge
-var testNumber = Math.floor(Math.random() * 2500) - 25;
-var testDivisible3 = testNumber % 3 === 0 && testNumber % 5 !== 0;
-console.log(testNumber);
+var randomNumber = Math.floor(Math.random() * 2500) - 25;
+var testDivisible3 = randomNumber % 3 === 0 && randomNumber % 5 !== 0;
+console.log(randomNumber);
 console.log(testDivisible3);
 
 // 5.  Challenge
@@ -41,7 +42,6 @@ console.log(testDivisible5);
 // 6. Challenge
 var creativeString =
     "Exploring the vast universe of code can lead to unexpected discoveries.";
-var randomNumOfStringLength = Math.floor(Math.random() * creativeString.length);
-var slicedString = creativeString.slice(randomNumOfStringLength);
-console.log(randomNumOfStringLength);
-console.log(slicedString);
+var randomIndex = Math.floor(Math.random() * creativeString.length);
+var slicedText = creativeString.slice(randomIndex + 1);
+console.log("Text after index " + randomIndex + ": " + slicedText);
