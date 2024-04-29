6d556d1df24fc8d722401ec80c488659478b4f9a
Jami Karimkulov <jami.karimkul@gmail.com>
2024-04-29 00:28:34 -0400

    Update assessment-8

--- assessment/assessment-8/solution.js
+++ assessment/assessment-8/solution.js
@@ -12,16 +12,33 @@ function isPrime(num) {
 // console.log(isPrime(4)); // Should return false
 // console.log(isPrime(29)); // Should return true
 
-// 2. Challenge
-function listPrimes(n) {
-    var result = 0;
+// 2. Challenge ???????????????
+// function listPrimes(n) {
+//     var result = 0;
+//     for (var i = 1; i <= n; i++) {
+//         console.log(i);
+//         if (n % i !== 0) {
+//             continue;
+//         }
+//         return n;
+//     }
+// }
+// console.log(listPrimes(10)); // Should return "2, 3, 5, 7"
+// listPrimes(20); // Should return "2, 3, 5, 7, 11, 13, 17, 19"
+// listPrimes(1); // Should return ""
+// listPrimes(2); // Should return "2"
 
-    for (var i = 2; i < n; i++) {
-        if (n % i === 0) result += i.toString();
-    }
-    return result;
-}
-console.log(listPrimes(10)); // Should return "2, 3, 5, 7"
-listPrimes(20); // Should return "2, 3, 5, 7, 11, 13, 17, 19"
-listPrimes(1); // Should return ""
-listPrimes(2); // Should return "2"
+// 3. Challenge ??????????????????
+// function generateRandomString16() {
+//     var strChars =
+//         "ABCDEFGHIJKLMNOPQRSTUVWabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+~:;.,/|[]{}";
+//     var strLength = strChars.length;
+//     var randomCharIdx = Math.floor(Math.random() * strLength + 1);
+//     console.log(randomCharIdx);
+//     var randomChar = strChars[randomCharIdx];
+//     console.log(randomChar + randomChar);
+//     for (var i = 0; i <= 16; i++) {
+
+//     }
+// }
+generateRandomString16();
