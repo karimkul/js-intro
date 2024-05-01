2db4969d78fb83fe54e92f342b7bd37c82d0347d
Jami Karimkulov <jami.karimkul@gmail.com>
2024-05-01 12:55:26 -0400

    Update assignment-18

--- assignments/assignment-18/assignment18.js
+++ assignments/assignment-18/assignment18.js
@@ -2,14 +2,17 @@
 function countDivisibleByThreeOrFive(n) {
     var divisibleNum = 0;
     for (var i = 1; i < n; i++) {
-        if (i % 3 === 0 && i % 5 !== 0) divisibleNum += 1;
-        else if (i % 5 === 0 && i % 3 !== 0) divisibleNum += 1;
+        if (i % 3 === 0 && i % 5 !== 0) {
+            divisibleNum += 1;
+        } else if (i % 5 === 0 && i % 3 !== 0) {
+            divisibleNum += 1;
+        }
     }
     return divisibleNum;
 }
-// console.log(countDivisibleByThreeOrFive(15)); // Outputs: 6 (3, 5, 6, 9, 10, 12)
-// console.log(countDivisibleByThreeOrFive(10)); // Outputs: 4 (3, 5, 6, 9)
-// console.log(countDivisibleByThreeOrFive(30)); // Outputs: 12 (3, 5, 6, 9, 10, 12, 18, 20, 21, 24, 27)
+console.log(countDivisibleByThreeOrFive(15)); // Outputs: 6 (3, 5, 6, 9, 10, 12)
+console.log(countDivisibleByThreeOrFive(10)); // Outputs: 4 (3, 5, 6, 9)
+console.log(countDivisibleByThreeOrFive(30)); // Outputs: 12 (3, 5, 6, 9, 10, 12, 18, 20, 21, 24, 27)
 
 // 2. sumUpToBoth
 function sumUpToBoth(num1, num2) {
@@ -24,9 +27,9 @@ function sumUpToBoth(num1, num2) {
     return sumUpNum1 + sumUpNum2;
 }
 
-// console.log(sumUpToBoth(5, 7)); // Outputs: 43 (1+2+3+4+5 + 1+2+3+4+5+6+7)
-// console.log(sumUpToBoth(3, 4)); // Outputs: 16 (1+2+3 + 1+2+3+4)
-// console.log(sumUpToBoth(6, 3)); // Outputs: 27 (1+2+3+4+5+6 + 1+2+3)
+console.log(sumUpToBoth(5, 7)); // Outputs: 43 (1+2+3+4+5 + 1+2+3+4+5+6+7)
+console.log(sumUpToBoth(3, 4)); // Outputs: 16 (1+2+3 + 1+2+3+4)
+console.log(sumUpToBoth(6, 3)); // Outputs: 27 (1+2+3+4+5+6 + 1+2+3)
 
 // 3. sumOfSquares
 function sumOfSquares(n) {
@@ -36,11 +39,40 @@ function sumOfSquares(n) {
     }
     return sum;
 }
-// console.log(sumOfSquares(3)); // Outputs: 14 (1^2 + 2^2 + 3^2)
-// console.log(sumOfSquares(5)); // Outputs: 55 (1^2 + 2^2 + 3^2 + 4^2 + 5^2)
-// console.log(sumOfSquares(2)); // Outputs: 5 (1^2 + 2^2)
+console.log(sumOfSquares(3)); // Outputs: 14 (1^2 + 2^2 + 3^2)
+console.log(sumOfSquares(5)); // Outputs: 55 (1^2 + 2^2 + 3^2 + 4^2 + 5^2)
+console.log(sumOfSquares(2)); // Outputs: 5 (1^2 + 2^2)
 
-// 4. countPerfectSquares ?????????
+// 4. countPerfectSquares
+function sumUpDigits(num) {
+    if (num < 10) {
+        return num;
+    }
+    var strNum = num.toString();
+    var sum = 0;
+    sum += parseInt(strNum[0]) + parseInt(strNum[1]);
+    if (strNum.length === 3) {
+        sum += parseInt(strNum[2]);
+    }
+    return sum;
+}
+console.log(sumUpDigits(12));
+console.log(sumUpDigits(999));
+console.log(sumUpDigits(1));
+
+function countDigitsSumDivisibleByThree(num1, num2) {
+    var counter = 0;
+    for (var i = num1; i <= num2; i++) {
+        var sum = sumUpDigits(i);
+        if (sum % 3 === 0) {
+            counter++;
+        }
+    }
+    return counter++;
+}
+console.log(countDivisibleByThreeOrFive(15)); // Outputs: 6 (3, 5, 6, 9, 10, 12)
+console.log(countDivisibleByThreeOrFive(10)); // Outputs: 4 (3, 5, 6, 9)
+console.log(countDivisibleByThreeOrFive(30)); // Outputs: 11 (3, 5, 6, 9, 10, 12, 18, 20, 21, 24, 27)
 
 // 5. countDigitsSumDivisibleByThree
 function countDigitsSumDivisibleByThree(num1, num2) {
@@ -50,4 +82,4 @@ function countDigitsSumDivisibleByThree(num1, num2) {
     }
     return result;
 }
-// console.log(countDigitsSumDivisibleByThree(7, 12)); // Outputs: 2
+console.log(countDigitsSumDivisibleByThree(7, 12)); // Outputs: 2
